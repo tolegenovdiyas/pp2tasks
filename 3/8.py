38812273ac12823bb8d3a96137120cee27dedac5
tolegenovdiyas <145113148+tolegenovdiyas@users.noreply.github.com>
2023-10-06 00:23:44 +0600

    Delete 3/8.py

--- 3/8.py
+++
@@ -1,9 +0,0 @@
-input_str = input()
-A = input_str.split()
-unique_elements = []
-count = 0
-for element in A:
-    if element not in unique_elements:
-        unique_elements.append(element)
-        count += 1
-print(count)
